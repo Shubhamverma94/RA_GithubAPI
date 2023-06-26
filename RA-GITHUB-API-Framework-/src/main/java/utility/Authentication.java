10fba62f674faeecc43c985e44127df0a1b2f1f0
Shubham Verma <imkannu.shubham@gmail.com>
2023-06-26 18:19:49 +0530

    adding project

---
+++ RA-GITHUB-API-Framework-/src/main/java/utility/Authentication.java
@@ -0,0 +1,11 @@
+package utility;
+
+public class Authentication {
+	
+	public final static String bearer_token = "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";
+	
+	public static String getbearertoken() {
+		return bearer_token;
+	}
+
+}
